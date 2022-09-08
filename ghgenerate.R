75dabba7ad6984c5a1f5a177f06d3a8b84cd9736
MartinSchobben <martin.schobben@hotmail.com>
2022-09-08 10:46:00 +0200

    uu proposal

--- ghgenerate.R
+++
@@ -1,8 +0,0 @@
-proposal <- "FAIRification of lab data" 
-proposal.file <- "isc-proposal.Rmd"
-author <- "Martin Schobben"
-
-rmarkdown::render(proposal.file, output_format="bookdown::html_document2",
-                  output_dir="out", quiet=TRUE)
-rmarkdown::render(proposal.file, output_format="bookdown::pdf_book",
-                  output_dir="out", quiet=TRUE)
